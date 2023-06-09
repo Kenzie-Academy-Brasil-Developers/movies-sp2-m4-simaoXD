be60ea8b94bd1fc18fced2aebf49068e7a51f839
Pedro Henrique <pedrosimao.m@gmail.com>
2023-06-09 19:35:06 -0300

    file .env add

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { config } from "dotenv";
 import express, { Application, json } from "express";
 
 import { verifyNameMovieExist, verifyMovieExist, filterCategoryMovies } from "./middlewares";
@@ -8,8 +9,7 @@ import { createMovie, deleteMovie, findMovie, readMoviesAll, updateMovie } from 
 const app: Application = express();
 app.use(json());
 
-const PORT: number = 3000;
-const sendMsg: string = `Server running on http://localhost:${PORT}`;
+const sendMsg: string = `Server running on http://localhost:${process.env.PORT}`;
 
 app.get("/movies", filterCategoryMovies, readMoviesAll);
 app.get("/movies/:id", verifyMovieExist, findMovie);
@@ -17,7 +17,7 @@ app.post("/movies", verifyNameMovieExist, createMovie);
 app.patch("/movies/:id", verifyMovieExist, verifyNameMovieExist, updateMovie);
 app.delete("/movies/:id", verifyMovieExist, deleteMovie);
 
-app.listen(PORT, async () => {
+app.listen(process.env.PORT, async () => {
   await startDataBase();
   console.log(sendMsg);
 });
--- src/database.ts
+++ src/database.ts
@@ -1,14 +1,17 @@
 import { Client } from "pg";
+import "dotenv/config";
 
 const client: Client = new Client({
-  user: "megazord",
-  host: "localhost",
-  port: 5432,
-  password: "12345",
-  database: "movies_database",
+  user: process.env.DB_USER!,
+  host: process.env.DB_HOST!,
+  port: Number(process.env.DB_PORT!),
+  password: process.env.DB_PASSWORD!,
+  database: process.env.DB!,
 });
+
 const startDataBase = async (): Promise<void> => {
   await client.connect();
   console.log("Database Connected Success");
 };
+
 export { startDataBase, client };
